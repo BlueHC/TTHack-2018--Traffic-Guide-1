9b11b18e0ff7763ec1a6fd15af71833dcdcd9caf
Tilman Ihrig <ihritil@tzi.de>
2018-04-21 12:00:09 +0200

    Add small doc to weather type IDs

--- weatherApi.py
+++ weatherApi.py
@@ -3,6 +3,18 @@ import urllib.parse
 import json
 
 def currentWeatherData():
+    """
+    TypeIDs: https://openweathermap.org/weather-conditions
+             2xx: Thunderstorm
+             3xx: Drizzle
+             5xx: Rain
+             6xx: Snow
+             7xx: Atmosphere
+             800: Clear
+             80x: Clouds
+             90x: Extreme
+             9xx: Misc
+    """
     url = 'https://api.openweathermap.org/data/2.5/weather?q=Hamburg&APPID=b5b4fb75e69eaf5754f4412ca9700d1b'
     f = urllib.request.urlopen(url)
     payload = f.read().decode('utf-8')
