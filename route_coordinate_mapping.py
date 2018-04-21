6cb5892b61bae8a4217ee990dbfc211132837af8
Tilman Ihrig <ihritil@tzi.de>
2018-04-21 18:07:32 +0200

    Fix bus station finding

--- route_coordinate_mapping.py
+++ route_coordinate_mapping.py
@@ -9,22 +9,23 @@ class HVVCoordinateMapper:
         self.df = None
         self.stop_to_index = {}
         self.lat_lon_to_index = {}
-        self.index_to_lat = {}
-        self.index_to_lon = {}
+        self.index_to_lat_lon = {}
         self.bike_coordinates = []
         self._load_bus_data()
         self._load_bike_data()
 
     def _load_bus_data(self, file="data/stops.txt"):
+        print('Loading bus data')
         self.df = pd.read_csv(filepath_or_buffer=file, sep=",")
         for i, row in self.df.iterrows():
             # only record coordinates of 'parent stations'
             if isinstance(row["parent_station"], float) and math.isnan(row["parent_station"]):
                 self.stop_to_index[row["stop_name"]] = i
                 self.lat_lon_to_index[(row["stop_lat"], row["stop_lon"])] = i
-        print(len(self.stop_to_index))
+                self.index_to_lat_lon[i] = (row["stop_lat"], row["stop_lon"])
 
     def _load_bike_data(self, file='data/1-StadtRAD_Hamburg_Stationen.gpx'):
+        print('Loading bike data')
         with open(file, 'r') as f:
             gpx = gpxpy.parse(f)
         for p in gpx.waypoints:
@@ -57,8 +58,7 @@ class HVVCoordinateMapper:
     def bus_stations_in_range(self, lat_start, lon_start, range=0.005):
         stations = []
         for name, index in self.stop_to_index.items():
-            lat = self.index_to_lat[index]
-            lon = self.index_to_lon[index]
+            (lat, lon) = self.index_to_lat_lon[index]
             dist = self.get_distance(lat_start, lon_start, lat, lon)
             if dist < range:
                 stations.append((name, dist))
