54fa262209c227738f81f6984a4bdd51af718838
Tilman Ihrig <ihritil@tzi.de>
2018-04-22 04:20:43 +0200

    Add early printing of features

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Main:
         self.generator = DataGenerator()
 
     def add_disruption(self, begin, end, time=12):
+        features_printed=False
         weather = self.generator.generate_weather()
         affected_routes = self.generator.filter_disrupted_routes(begin, end)
         walkers = 0
@@ -15,6 +16,7 @@ class Main:
         car_drivers = 0
         pt_users = 0
         routes = self.generator.routes["stations"][affected_routes]
+        norm_fact = 2 * len(routes)
         for index, route in enumerate(routes):
             got_first_stop = False
             for i, stop in enumerate(route):
@@ -33,12 +35,15 @@ class Main:
                         stations_to_target = reversed(route[:i])
                         for station in stations_to_target:
                             features, (walking_prob, bike_prob, car_prob, pt_prob) = self.generator.generate_features_for_dest(stop, station, time, weather=weather)
+                            if not features_printed:
+                                print("Features: %s" % features)
+                                features_printed = True
                             walkers += walking_prob * station_prob_mapping[station]
                             bikers += bike_prob * station_prob_mapping[station]
                             car_drivers += car_prob * station_prob_mapping[station]
                             pt_users += pt_prob * station_prob_mapping[station]
                         got_first_stop = True
-        return (features, np.array([walkers / len(routes), bikers / len(routes), car_drivers / len(routes), pt_users / len(routes)]))
+        return (features, np.array([walkers / norm_fact, bikers / norm_fact, car_drivers / norm_fact, pt_users / norm_fact]))
 
 if __name__ == "__main__":
     main = Main()
