745a762df3300e275e89cbea031afeb89392467f
Marlon Flügge <marlonfl@fb3.uni-bremen.de>
2018-04-22 05:39:18 +0200

    add try except block to main.py

--- main.py
+++ main.py
@@ -17,33 +17,37 @@ class Main:
         pt_users = 0
         routes = self.generator.routes["stations"][affected_routes]
         norm_fact = 2 * len(routes)
-        for index, route in enumerate(routes):
-            got_first_stop = False
-            for i, stop in enumerate(route):
-                if stop == begin or stop == end:
-                    if got_first_stop:
-                        station_prob_mapping = self.generator.generate_prob_mapping(stop, route[-1], affected_routes[index])
-                        stations_to_target = route[i+1:]
-                        for station in stations_to_target:
-                            features, (walking_prob, bike_prob, car_prob, pt_prob) = self.generator.generate_features_for_dest(stop, station, time, weather=weather)
-                            walkers += walking_prob * station_prob_mapping[station]
-                            bikers += bike_prob * station_prob_mapping[station]
-                            car_drivers += car_prob * station_prob_mapping[station]
-                            pt_users += pt_prob * station_prob_mapping[station]
-                    else:
-                        station_prob_mapping = self.generator.generate_prob_mapping(stop, route[0], affected_routes[index])
-                        stations_to_target = reversed(route[:i])
-                        for station in stations_to_target:
-                            features, (walking_prob, bike_prob, car_prob, pt_prob) = self.generator.generate_features_for_dest(stop, station, time, weather=weather)
-                            if not features_printed:
-                                print("Features: %s" % features)
-                                features_printed = True
-                            walkers += walking_prob * station_prob_mapping[station]
-                            bikers += bike_prob * station_prob_mapping[station]
-                            car_drivers += car_prob * station_prob_mapping[station]
-                            pt_users += pt_prob * station_prob_mapping[station]
-                        got_first_stop = True
-        return (features, np.array([walkers / norm_fact, bikers / norm_fact, car_drivers / norm_fact, pt_users / norm_fact]))
+        try:
+            for index, route in enumerate(routes):
+                got_first_stop = False
+                for i, stop in enumerate(route):
+                    if stop == begin or stop == end:
+                        if got_first_stop:
+                            station_prob_mapping = self.generator.generate_prob_mapping(stop, route[-1], affected_routes[index])
+                            stations_to_target = route[i+1:]
+                            for station in stations_to_target:
+                                features, (walking_prob, bike_prob, car_prob, pt_prob) = self.generator.generate_features_for_dest(stop, station, time, weather=weather)
+                                walkers += walking_prob * station_prob_mapping[station]
+                                bikers += bike_prob * station_prob_mapping[station]
+                                car_drivers += car_prob * station_prob_mapping[station]
+                                pt_users += pt_prob * station_prob_mapping[station]
+                        else:
+                            station_prob_mapping = self.generator.generate_prob_mapping(stop, route[0], affected_routes[index])
+                            stations_to_target = reversed(route[:i])
+                            for station in stations_to_target:
+                                features, (walking_prob, bike_prob, car_prob, pt_prob) = self.generator.generate_features_for_dest(stop, station, time, weather=weather)
+                                if not features_printed:
+                                    print("Features: %s" % features)
+                                    features_printed = True
+                                walkers += walking_prob * station_prob_mapping[station]
+                                bikers += bike_prob * station_prob_mapping[station]
+                                car_drivers += car_prob * station_prob_mapping[station]
+                                pt_users += pt_prob * station_prob_mapping[station]
+                            got_first_stop = True
+            return (features, np.array([walkers / norm_fact, bikers / norm_fact, car_drivers / norm_fact, pt_users / norm_fact]))
+        except:
+            print("Error for connection {} to {}".format(begin, end))
+            return [], []
 
     def return_json(self, json_text, time=12):
         begin = ""
